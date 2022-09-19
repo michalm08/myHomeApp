9a72e75e6deaa0e0a6dc4d81b63f12cbcb07b9d1
michalm08 <michalski@gmail.com>
2022-09-19 18:59:16 +0200

    feat(front): show list of users

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -11,12 +11,28 @@ interface UserDataInterface {
   avatar: string;
 }
 
+const solrtByLastName = (userA: UserDataInterface, userB: UserDataInterface) => {
+  if (userA.last_name < userB.last_name) {
+    return -1;
+  }
+  if (userA.last_name > userB.last_name) {
+    return 1;
+  }
+  return 0;
+};
+
 const MainPage = () => {
   const [usersData, setUsersData] = useState<UserDataInterface[]>([]);
 
+  const sortArray = (arrayToSort: UserDataInterface[]) => {
+    let sortedArray = arrayToSort.sort(solrtByLastName);
+    console.log(sortedArray);
+    return sortedArray;
+  };
+  // git commit -m 'feat(front): show list of user
   useEffect(() => {
     getUsersData().then((res) => {
-      setUsersData(res.data);
+      setUsersData(sortArray(res.data));
     });
   }, []);
 
@@ -24,6 +40,12 @@ const MainPage = () => {
     <div className="MainPage">
       <h1>MainPage</h1>
       <p onClick={() => console.log(usersData[0])}>klik</p>
+      {usersData.map((user) => (
+        <div>
+          <img src={user.avatar} alt={`Avatar of: ${user.first_name} ${user.last_name}`} />
+          <p>{user.first_name} | {user.last_name}</p>
+        </div>
+      ))}
     </div>
   );
 };
