20d5aea4134f76a645481e69638d4814cc258dc6
michalm08 <michalski@gmail.com>
2022-09-19 18:14:00 +0200

    feat(front): create "mainPage"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div className="App">
       <p>Tea Code</p>
+      <MainPage />
     </div>
   );
 }
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './MainPage.scss';
+
+const MainPage = () => {
+  return <div className="MainPage">MainPage</div>;
+};
+
+export default MainPage;
